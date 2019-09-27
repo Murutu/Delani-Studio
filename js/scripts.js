f4fe261b1dc3e0c96742dd6dc9f1d5798ef7425c
Murutu <murutupeter@email.com>
2019-09-27 16:41:21 +0300

    add product

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".click").click(function() {
+        $("#k-hidden").toggle();
+        $("#d1-show").toggle()
+    });
+});
+
+$(document).ready(function() {
+    $(".click1").click(function() {
+        $("#l-hidden").toggle();
+        $("#d2-show").toggle()
+    });
+});
