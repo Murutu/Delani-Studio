f23f10dc6afa4efa8c80c956e247bc67b84220da
Murutu <murutupeter@email.com>
2019-10-01 10:08:33 +0300

    made changes to portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
 $(document).ready(function() {
     $(".click").click(function() {
         $("#k-hidden").toggle();
-        $("#d1-show").toggle()
+        $("#d1-show").toggle();
     });
 });
 
 $(document).ready(function() {
     $(".click1").click(function() {
         $("#l-hidden").toggle();
-        $("#d2-show").toggle()
+        $("#d2-show").toggle();
     });
 });
 
 $(document).ready(function() {
     $(".click3").click(function() {
-        $("#m-hidden").toggle()
-            $("#d4-show").toggle()
-        });
+        $("#m-hidden").toggle();
+            $("#d4-show").toggle();
     });
+});
+
+$(document).ready(function() {
+$(".namee").hide()
+$(".b1").hover(function(){
+$(this).find(".namee").toggle();
+        });
+});
+
+$(document).ready(function() {
+    $(".frm").submit(function(){
+        var name=$("input#name").val();
+        alert("Thank you for reaching out to us " +name +".We have received your feedback.");
+
+    });
+});
+
+
+
+
+
+
+
+
